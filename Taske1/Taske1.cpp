f66603bbd753bfd8885d99ebf9217ffcdf02db6b
ACER <ACER@DESKTOP-UFQN3AN>
2026-09-24 16:14:43 +0300

    Add project files.

---
+++ Taske1/Taske1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int first_value, second_value, product;
+    cout << "Enter the first value: ";
+    cin >> first_value;
+    cout << "Enter the second value: ";
+    cin >> second_value;
+    product = first_value * second_value;
+    cout << "The product is: " << product;
+
+    return 0;
+}
